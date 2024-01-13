bf9dc477ac1e39561fe84356be538a1da265f63f
Max Ruuen <max@madebyruuen.com>
2024-01-13 13:08:06 +1100

    Add SearchForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { SearchMode, SpotifyUser } from "./types";
+import { SearchFormInputs, SearchMode, SpotifyUser } from "./types";
 import { useState } from "react";
 import PageHeader from "./components/PageHeader";
 import PlaylistCard from "./components/PlaylistCard";
 import SearchModeSelect from "./components/SearchModeSelect";
+import SearchForm from "./components/SearchForm";
 
 function App() {
   const [searchMode, setSearchMode] = useState<SearchMode>("public");
   const [user, setUser] = useState<SpotifyUser | null>(null);
+
+  function handlePlaylistSearch(data: SearchFormInputs): void {
+    console.log(data);
+  }
 
   function changeSearchMode(e: React.MouseEvent<HTMLButtonElement>) {
     switch (e.currentTarget.dataset.mode) {
@@ -28,6 +33,7 @@ function App() {
           isLoggedIn={user}
           handleClick={changeSearchMode}
         />
+        <SearchForm handlePlaylistSearch={handlePlaylistSearch} />
         {searchMode === "public" && (
           <PlaylistCard
             coverImage="https://images.unsplash.com/photo-1704714673347-113cfe46485e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTcwNDg1NjkxNA&ixlib=rb-4.0.3&q=80&w=300"
---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,51 @@
+import { useForm } from "react-hook-form";
+import { SearchFormInputs } from "../types";
+import Button from "./Button";
+
+interface SearchFormProps {
+  handlePlaylistSearch: (data: SearchFormInputs) => void;
+}
+
+function SearchForm({ handlePlaylistSearch }: SearchFormProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SearchFormInputs>();
+
+  return (
+    <form
+      action="get"
+      autoComplete="off"
+      onSubmit={handleSubmit(handlePlaylistSearch)}
+      className="p-5 bg-slate-800 border border-slate-700 rounded-md shadow-lg"
+    >
+      <label className="sr-only" htmlFor="playlistUrl">
+        Public playlist to export
+      </label>
+      <div className="space-y-3">
+        <input
+          type="text"
+          {...register("playlistUrl", {
+            required:
+              "Field is empty; please enter a valid Spotify playlist URL.",
+          })}
+          placeholder="Enter a public playlist URL..."
+          className="block w-full py-2 px-3 bg-slate-900 border border-slate-700 text-slate-100 rounded-md"
+        />
+
+        <Button
+          type="submit"
+          text="Find..."
+          colourScheme="light"
+          isFullWidth={true}
+        />
+      </div>
+      {errors.playlistUrl && (
+        <span className="block mt-3">{errors.playlistUrl.message}</span>
+      )}
+    </form>
+  );
+}
+
+export default SearchForm;
