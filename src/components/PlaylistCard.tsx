61fb45db35a9bb4f10493bbae2ec1ec327b04752
Max Ruuen <max@madebyruuen.com>
2024-01-17 20:56:32 +1100

    Add user error handling to PlaylistCard

--- src/components/PlaylistCard.tsx
+++ src/components/PlaylistCard.tsx
@@ -1,39 +1,55 @@
+import { ResponseError } from "../types";
 import ButtonLink from "./ButtonLink";
 
 interface PlaylistCardProps {
   coverImage: string;
   title: string;
   author: string;
+  errorState: ResponseError | null;
 }
 
 // TODO: Update this component to respond to error props
-function PlaylistCard({ title, author, coverImage }: PlaylistCardProps) {
+function PlaylistCard({
+  title,
+  author,
+  coverImage,
+  errorState,
+}: PlaylistCardProps) {
   return (
     <article className="flex max-w-md h-full min-h-32 md:max-w-none mx-auto bg-slate-800 border border-slate-700 rounded-md shadow-lg">
-      <img
-        src={coverImage}
-        alt=""
-        className="max-w-32 rounded-l-md object-cover"
-      />
-      <div className="flex flex-1 p-5 flex-col gap-3">
-        <div>
-          <h4 className="font-medium text-slate-100">{title}</h4>
-          <a
-            className="break-all hover:text-slate-100 transition-colors"
-            href="#"
-          >
-            @{author}
-          </a>
+      {errorState && (
+        <div className="p-5 m-auto text-center text-xl">
+          <p>{errorState.error.message}</p>
         </div>
-        <ul className="flex flex-wrap gap-2 mt-auto">
-          <li>
-            <ButtonLink text="CSV" href="#" colourScheme="light" />
-          </li>
-          <li>
-            <ButtonLink text="JSON" href="#" colourScheme="light" />
-          </li>
-        </ul>
-      </div>
+      )}
+      {!errorState && (
+        <>
+          <img
+            src={coverImage}
+            alt=""
+            className="max-w-32 rounded-l-md object-cover"
+          />
+          <div className="flex flex-1 p-5 flex-col gap-3">
+            <div>
+              <h4 className="font-medium text-slate-100">{title}</h4>
+              <a
+                className="break-all hover:text-slate-100 transition-colors"
+                href="#"
+              >
+                @{author}
+              </a>
+            </div>
+            <ul className="flex flex-wrap gap-2 mt-auto">
+              <li>
+                <ButtonLink text="CSV" href="#" colourScheme="light" />
+              </li>
+              <li>
+                <ButtonLink text="JSON" href="#" colourScheme="light" />
+              </li>
+            </ul>
+          </div>
+        </>
+      )}
     </article>
   );
 }
--- src/components/PublicPlaylistView.tsx
+++ src/components/PublicPlaylistView.tsx
@@ -23,10 +23,9 @@ function PublicPlaylistView() {
       {/* TODO: isLoading passed as prop to SearchForm, loader displayed within form submit btn */}
       <SearchForm handlePlaylistSearch={handlePlaylistSearch} />
       {isLoading && <span>This is a test loading message...</span>}
-      {/* TODO: Display of errors to be handled by PlaylistCard and passed via prop here */}
-      {!isLoading && errorState && <span>{JSON.stringify(errorState)}</span>}
       {playlist && (
         <PlaylistCard
+          errorState={errorState}
           title={playlist.name}
           author={playlist.owner.display_name}
           coverImage="https://images.unsplash.com/photo-1703401895640-bf1b9f6c803e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTcwNTIxMzQ1MA&ixlib=rb-4.0.3&q=80&w=300"
