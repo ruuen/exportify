878a325db1668f181fcbc532ad00b17694ccb482
Max Ruuen <max@madebyruuen.com>
2024-01-28 14:00:05 +1100

    Map Vite base var to Netlify URL env var Netlify provides URL of deploy in different contexts Dev will be localhost:8888 Prod will be main HTTPS url of site Deploy previews will be HTTPS url of that preview This is then used in queryBackend func for querying right backend URL based on deploy

--- src/utils/queryBackend.ts
+++ src/utils/queryBackend.ts
@@ -1,3 +1,6 @@
+/** Dynamic URL of site from Vite base url */
+const deployUrl = new URL(import.meta.url).origin;
+
 /** Type thrown from operational errors during API calls */
 class ApiError extends Error {}
 
@@ -6,7 +9,7 @@ async function queryBackend<T>(
   endpoint: string,
   customOptions?: RequestInit
 ): Promise<T> {
-  const baseUrl = "http://localhost:8888/api";
+  const baseUrl = `${deployUrl}/api`;
   const options: RequestInit = {
     method: "GET",
     headers: {
