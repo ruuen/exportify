cd7b102f66b05c491cd34ed14493ec119a911fd1
Max Ruuen <max@madebyruuen.com>
2024-02-02 00:46:15 +1100

    Change user state obj to isLoggedIn bool, conditionally show login/logout buttons Wire up login button to initialise and trigger auth process

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { SearchMode, SpotifyUser } from "./types";
+import { SearchMode } from "./types";
 import { useState } from "react";
 import PageHeader from "./components/PageHeader";
 import SearchModeSelect from "./components/SearchModeSelect";
 import ErrorBoundary from "./components/ErrorBoundary";
 import PublicPlaylistView from "./components/PublicPlaylistView";
 import PrivatePlaylistView from "./components/PrivatePlaylistView";
+import { checkUserLoggedIn } from "./utils/auth";
 
 function App() {
   const [searchMode, setSearchMode] = useState<SearchMode>("public");
-  const [user] = useState<SpotifyUser | null>(null);
+  const isLoggedIn = checkUserLoggedIn();
 
   function changeSearchMode(e: React.MouseEvent<HTMLButtonElement>) {
     switch (e.currentTarget.dataset.mode) {
@@ -23,11 +24,11 @@ function App() {
 
   return (
     <>
-      <PageHeader />
+      <PageHeader isLoggedIn={isLoggedIn} />
       <main className="mt-6 mx-auto space-y-6 lg:mt-10 lg:space-y-10 lg:max-w-6xl">
         <SearchModeSelect
           searchMode={searchMode}
-          isLoggedIn={user}
+          isLoggedIn={isLoggedIn}
           handleClick={changeSearchMode}
         />
         <ErrorBoundary fallback={<p>Something went wrong... testing</p>}>
