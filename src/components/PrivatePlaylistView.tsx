69065247ca8da792a039923d07caaf7e4bb1c256
Max Ruuen <max@madebyruuen.com>
2024-02-17 16:54:47 +1100

    Add error to PrivatePlaylistView if request cannot return data

--- src/components/PrivatePlaylistView.tsx
+++ src/components/PrivatePlaylistView.tsx
@@ -41,6 +41,13 @@ function PrivatePlaylistView() {
           <LoadingSpinner />
         </div>
       )}
+      {errorState && !isLoading && (
+        <div className="mx-auto">
+          <p>
+            Unable to fetch your playlists from Spotify API. Please try again.
+          </p>
+        </div>
+      )}
       {playlistElements && !isLoading && (
         <ul className="space-y-5 md:space-y-0 md:grid md:grid-cols-2 md:grid-flow-row md:auto-rows-min md:gap-5 lg:grid-cols-3">
           {playlistElements}
