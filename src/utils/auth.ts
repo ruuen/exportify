402c958cc50a3effc387269ea53cef5609b61c78
Max Ruuen <66315163+ruuen@users.noreply.github.com>
2024-02-17 11:54:56 +1100

    Fix client initSpotifyLogin func to use current deploy domain for nonce cookie (#11)
    Adjusted the deployUrl var to just parse into URL obj
    The window.location.assign redirect now references the origin of URL obj
    Cookie references hostname prop of URL obj

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,5 @@
 /** Dynamic URL of site from Vite base url */
-const deployUrl = new URL(import.meta.url).origin;
+const deployUrl = new URL(import.meta.url);
 
 /** Generate nonce and redirect user to backend login endpoint to start Spotify auth process */
 function initSpotifyLogin() {
@@ -7,11 +7,11 @@ function initSpotifyLogin() {
   const nonce = Math.floor(Math.random() * (99999999 - 10000000) + 10000000);
 
   // Store as cookie
-  const nonceCookie = `exportify-nonce=${nonce}; Path=/api; SameSite=lax; Max-Age=300; Domain=localhost`;
+  const nonceCookie = `exportify-nonce=${nonce}; Path=/api; SameSite=lax; Max-Age=300; Domain=${deployUrl.hostname}`;
   document.cookie = nonceCookie;
 
   // Redirect browser to /api/login to begin Spotify auth process
-  window.location.assign(`${deployUrl}/api/login`);
+  window.location.assign(`${deployUrl.origin}/api/login`);
 }
 
 /** Checks cookies to confirm whether user has an access token */
